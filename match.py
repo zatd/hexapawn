d4564e225db4face682c0a19e0a2401ed7f9e8f2
zatd <49304381+zatd@users.noreply.github.com>
2019-05-06 17:57:31 +0800

    Update match.py

--- match.py
+++ match.py
@@ -17,9 +17,72 @@ win condition:
 
 """
 
-import tkinter
-import sys
-    
+from tkinter import Tk, Label, Button, IntVar, W, E
+
+class GameBoardGUI:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Hexapawn")
+
+        self.total = 0
+        self.entered_number = 0
+
+        self.total_label_text = IntVar()
+        self.total_label_text.set((self.total)/10)
+        self.total_label1 = Label(master, textvariable=self.total_label_text)
+        self.total_label_text.set(self.total%10)
+        self.total_label2 = Label(master, textvariable=self.total_label_text)
+
+        self.label = Label(master, text="Move:")
+
+        self.button0 = Button(master, text=pieces[0], command=lambda: self.update(0))
+        self.button1 = Button(master, text=pieces[1], command=lambda: self.update(1))
+        self.button2 = Button(master, text=pieces[2], command=lambda: self.update(2))
+        self.button3 = Button(master, text=pieces[3], command=lambda: self.update(3))
+        self.button4 = Button(master, text=pieces[4], command=lambda: self.update(4))
+        self.button5 = Button(master, text=pieces[5], command=lambda: self.update(5))
+        self.button6 = Button(master, text=pieces[6], command=lambda: self.update(6))
+        self.button7 = Button(master, text=pieces[7], command=lambda: self.update(7))
+        self.button8 = Button(master, text=pieces[8], command=lambda: self.update(8))
+        
+        self.submit_button = Button(master, text="Submit", command=lambda: self.update("submit"))
+        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
+        self.quit_button = Button(master, text="Quit", command=lambda: self.update("quit"))
+
+        # LAYOUT
+        self.label.grid(row=0, column=0, sticky=W)
+        self.total_label1.grid(row=0, column=1, columnspan=1, sticky=W+E)
+        self.total_label2.grid(row=0, column=2, columnspan=1, sticky=W+E)
+
+        self.button0.grid(row=1, column=0, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button1.grid(row=1, column=1, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button2.grid(row=1, column=2, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button3.grid(row=2, column=0, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button4.grid(row=2, column=1, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button5.grid(row=2, column=2, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button6.grid(row=3, column=0, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button7.grid(row=3, column=1, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        self.button8.grid(row=3, column=2, sticky=W+E, columnspan=1, rowspan=1, padx=5, pady=5, ipadx=15, ipady=10)
+        
+        self.submit_button.grid(row=4, column=0, sticky=W+E, columnspan=3, rowspan=1, padx=15, pady=5)
+        self.reset_button.grid(row=5, column=0, sticky=W+E, columnspan=3, rowspan=1, padx=15, pady=5)
+        self.quit_button.grid(row=6, column=0, sticky=W+E, columnspan=3, rowspan=1, padx=15, pady=5)
+        
+    def update(self, method):
+        global move
+        if method in [0,1,2,3,4,5,6,7,8]:
+            self.total *= 10
+            self.total += (method+1)
+            if self.total >= 100: self.total %= 100
+        elif method == "submit":
+            move = self.total
+        elif method == "reset":
+            self.total = 0    
+        else: # quit
+            move = 'q'
+        self.total_label_text.set(self.total)
+
 def changeplayer():
     global player
     
@@ -31,13 +94,16 @@ def printgameboard():
     
     for i in range(0,7,3):
         print (pieces[i:i+3], slots[i:i+3])
-
+        
 def checkvalidity(move):
     global pieces, slots, player
     
     validity = True
     try:
-        if len(move) != 2: validity = False
+        move = str(move)
+        if len(move) != 2: 
+            print ("Error! Only choose two numbers!")
+            validity = False
         #move within range
         if move[0] and move[1] not in slots: validity = False
         #can only move 'x' or 'o'
@@ -129,7 +195,7 @@ def checkwin():
     winner = 0
     
 def main():
-    global pieces, slots, player, winner, expression
+    global pieces, slots, player, winner, expression, move
     pieces = ['x','x','x',' ',' ',' ','o','o','o']
     slots = ['1','2','3','4','5','6','7','8','9']
     player = 1
@@ -138,8 +204,12 @@ def main():
     printgameboard()
     
     while True:
-        move = input("Player "+ str(player) + "! Your move: ")
+        root = Tk()
+        move = GameBoardGUI(root)
+        root.mainloop()
+#        move = input("Player "+ str(player) + "! Your move: ")
         #quit game with 'q'
+        
         if move is 'q': 
             print ("Quit game!")
             break
